ee387784d9d7ad8cab57c93d447cb3d75a351361
leviportugal <102754345+leviportugal@users.noreply.github.com>
2023-03-24 17:04:19 -0300

    aula37

--- js/Game.js
+++ js/Game.js
@@ -19,8 +19,13 @@ class Game {
                                      // criar getCount no player!
     form = new Form();
     form.display();
-    
-    
+    car1 = createSprite (width/2-50,height-100)
+    car1.addImage (car1_img)
+    car1.scale = 0.07
+
+    car2 = createSprite (width/2+100,height-100)
+    car2.addImage (car2_img)
+    car2.scale = 0.07
   }
 
   // ATUALIZA NO BANCO DE DADOS O ESTADO DO JOGO PARA 1
@@ -28,12 +33,15 @@ class Game {
   handleElements() {
     form.hide(); // apaga o form
     form.titleImg.position(40, 50); // redefine o titulo
-    form.titleImg.class("gameTitleAfterEffect"); // novo estilo do titulo
+    //form.titleImg.class("gameTitleAfterEffect"); // novo estilo do titulo
   }
 
   play() {
-    
-
+    this.handleElements ()
+   if (playerCount===2){
+    image(track, 0, -height * 5, width, height * 6);
+    drawSprites()
+   }
 
 
   }
